6750aec5237a118365be81bdbb4109f0347e940a
Russell Nibbelink <russell.n@berkeley.edu>
2015-09-17 21:38:26 -0700

    h1 passes ag tests

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,10 @@
 class Foobar
   # Q4 CODE HERE
+    def self.baz(a)
+        total = 0
+        a = a.collect{|i| (i.to_i)+2}
+        a = a.uniq
+        a = a.select{|i| i%2 ==0 && i < 10}
+        return a.inject(:+)
+    end
 end
